24b238ca61a8b127d2e9d228bf8da35d5bc98ff7
Aahnika2001 <Aahnika@vtechsolllc.com>
2023-04-26 12:58:15 +0530

    updated src

---
+++ src/app/app.module.ts
@@ -0,0 +1,59 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
+
+import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { LandingComponent } from './landing/landing.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+
+import { HomeModule } from './home/home.module';
+import { LoginComponent } from './login/login.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { QuestionsComponent } from './questions/questions.component';
+import { ValidationComponent } from './validation/validation.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { SettingsComponent } from './settings/settings.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    PrivacyPolicyComponent,
+    LandingComponent,
+    ProfileComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent,
+    QuestionsComponent,
+    ValidationComponent,
+    ContactusComponent,
+    SettingsComponent,
+    
+  ],
+  
+  imports: [
+    BrowserModule,
+    NgbModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule,
+    AppRoutingModule,
+    HomeModule,
+    MDBBootstrapModule.forRoot(),
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
